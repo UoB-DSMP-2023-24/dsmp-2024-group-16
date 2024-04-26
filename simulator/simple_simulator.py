fce15638e26335c1e8be42d99cf8d66529ae134f
casperking666 <casperkingwang@gmail.com>
2024-04-26 02:21:56 +0100

    created an entire pipeline for simulation, tested some arima params

--- simulator/simple_simulator.py
+++ simulator/simple_simulator.py
@@ -1,17 +1,19 @@
 import pandas as pd
+import os
+from tqdm import tqdm
+import matplotlib.pyplot as plt
 
-# Load the CSV file to review its structure and content
-file_path = '../Tapes/UoB_Set01_2025-06-23tapes.csv'
-data = pd.read_csv(file_path)
 
-# Correcting the DataFrame column names based on the user's clarification
-data.columns = ['Timestamp', 'Price', 'Volume']
-
-# Display the corrected DataFrame structure
-data.head()
-
-# Define initial setup and constants
-initial_capital = 10000
+'''For a single file'''
+# # Load the CSV file to review its structure and content
+# file_path = '../Tapes/UoB_Set01_2025-06-23tapes.csv'
+# data = pd.read_csv(file_path)
+#
+# # Correcting the DataFrame column names based on the user's clarification
+# data.columns = ['Timestamp', 'Price', 'Volume']
+#
+# # Define initial setup and constants
+# initial_capital = 10000
 
 
 # Revised strategy implementations considering volume constraints
@@ -74,7 +76,46 @@ def volume_weighted_strategy_revised(data, initial_capital):
 
 
 # Re-run simulations with volume constraints
-momentum_profit_revised = momentum_trading_revised(data, initial_capital)
-volume_weighted_profit_revised = volume_weighted_strategy_revised(data, initial_capital)
+# momentum_profit_revised = momentum_trading_revised(data, initial_capital)
+# volume_weighted_profit_revised = volume_weighted_strategy_revised(data, initial_capital)
+#
+# print(momentum_profit_revised, volume_weighted_profit_revised)
 
-print(momentum_profit_revised, volume_weighted_profit_revised)
+
+
+# Load all the csv files in the directory
+directory_path = '../Tapes/'
+files = os.listdir(directory_path)
+
+# store the profit for each file and plot it where x-axis is the date in the file and y-axis is the profit
+profits_momentum = []
+profits_volume_weighted = []
+dates = []
+
+for file in tqdm(files):
+    file_path = directory_path + file
+    data = pd.read_csv(file_path)
+    data.columns = ['Timestamp', 'Price', 'Volume']
+    initial_capital = 10000
+
+    momentum_profit_revised = momentum_trading_revised(data, initial_capital)
+    volume_weighted_profit_revised = volume_weighted_strategy_revised(data, initial_capital)
+
+    profits_momentum.append(momentum_profit_revised)
+    profits_volume_weighted.append(volume_weighted_profit_revised)
+
+    # Extract the date from the file name
+    date = file.split('_')[2].split('tapes')[0]
+    dates.append(date)
+
+
+# Plot the profits
+plt.figure(figsize=(12, 6))
+plt.plot(dates, profits_momentum, label='Momentum Trading')
+plt.plot(dates, profits_volume_weighted, label='Volume-Weighted Strategy')
+plt.xlabel('Date')
+plt.ylabel('Profit')
+plt.title('Profit Comparison for Different Strategies')
+plt.legend()
+plt.show()
+
